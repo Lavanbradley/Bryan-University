e90d5fa48a2636599ea4c08409abda28e77cae32
Lavanbradley <79238572+Lavanbradley@users.noreply.github.com>
2021-10-15 19:06:16 -0700

    .

---
+++ fsw-135/week3/Rock the vote/routes/issuesRouter.js
@@ -0,0 +1,75 @@
+const express = require("express")
+const issuesRouter = express.Router()
+const Issue = require('../models/issue.js')
+
+
+issuesRouter
+
+.get("/", (req, res, next) => {
+  Issue.find((err, issues) => {
+    if(err){
+      res.status(500)
+      return next(err)
+    }
+    return res.status(200).send(issues)
+  })
+})
+
+.get("/:issueId", (req, res, next) => {
+  Issue.findOne(
+    { _id: req.params.issueId },
+    req.body,
+    { new: true },
+    (err, updatedIssue) => {
+      if(err){
+        res.status(500)
+        return next(err)
+      }
+      return res.status(201).send(updatedIssue)
+    }
+  )
+})
+
+.post("/", (req, res, next) => {
+  req.body.user = req.user._id
+  const newIssue = new Issue(req.body)
+  newIssue.save((err, savedIssue) => {
+    if(err){
+      res.status(500)
+      return next(err)
+    }
+    return res.status(201).send(savedIssue)
+  })
+})
+
+
+.delete("/:issueId", (req, res, next) => {
+  Issue.findOneAndDelete(
+    { _id: req.params.issueId },
+    (err, deletedIssue) => {
+      if(err){
+        res.status(500)
+        return next(err)
+      }
+      return res.status(200).send(`Successfully delete issue: ${deletedIssue.title}`)
+    }
+  )
+})
+
+.put("/:issueId", (req, res, next) => {
+  Issue.findOneAndUpdate(
+    { _id: req.params.issueId },
+    req.body,
+    { new: true },
+    (err, updatedIssue) => {
+      if(err){
+        res.status(500)
+        return next(err)
+      }
+      return res.status(201).send(updatedIssue)
+    }
+  )
+})
+
+
+module.exports = issuesRouter
