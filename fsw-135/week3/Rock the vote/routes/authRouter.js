be3080e2535fa2da39f2ed4505cff39fb9642142
Lavanbradley <79238572+Lavanbradley@users.noreply.github.com>
2021-10-30 11:04:45 -0700

    .

--- fsw-135/week3/Rock the vote/routes/authRouter.js
+++ fsw-135/week3/Rock the vote/routes/authRouter.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken')
 
 // Signup
 authRouter.post("/signup", (req, res, next) => {
+  const failedLogin = 'Username or password is incorrect'
   User.findOne({ username: req.body.username.toLowerCase() }, (err, user) => {
     if(err){
       res.status(500)
@@ -21,14 +22,15 @@ authRouter.post("/signup", (req, res, next) => {
         return next(err)
       }
                             // payload,            // secret
-      const token = jwt.sign(savedUser.toObject(), process.env.SECRET)
-      return res.status(201).send({ token, user: savedUser })
+      const token = jwt.sign(savedUser.withoutPassword(), process.env.SECRET)
+      return res.status(201).send({ token, user: savedUser.withoutPassword() })
     })
   })
 })
 
 // Login
 authRouter.post("/login", (req, res, next) => {
+  const failedLogin = 'Username or password is incorrect'
   User.findOne({ username: req.body.username.toLowerCase() }, (err, user) => {
     if(err){
       res.status(500)
@@ -36,16 +38,23 @@ authRouter.post("/login", (req, res, next) => {
     }
     if(!user){
       res.status(403)
-      return next(new Error("Username or Password are incorrect"))
+      return next(new Error(failedLogin))
     }
-    if(req.body.password !== user.password){
-      res.status(403)
-      return next(new Error("Username or Password are incorrect"))
-    }
-    const token = jwt.sign(user.toObject(), process.env.SECRET)
-    return res.status(200).send({ token, user })
+    user.checkPassword(req.body.password, (err, isMatch) =>{
+      if(err){
+        res.status(403)
+        return next(new Error(failedLogin))
+      }
+      if(!isMatch){
+        res.status(403)
+        return next(new Error(failedLogin))
+      }
+      const token = jwt.sign(user.withoutPassword(), process.env.SECRET)
+      return res.status(200).send({ token, user: user.withoutPassword() })
+    })
   })
 })
+
 
 
 module.exports = authRouter
